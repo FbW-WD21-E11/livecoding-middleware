04973104bf7e8adbabea76257227d6030be055c0
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2022-09-14 14:28:51 +0200

    changes

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,5 +1,6 @@
 export const authenticate = (req, res, next) => {
     console.log("query paremeters", req.query);
+    //check for parameter secret in the body or the query param
     if(req.body.secret == null && req.query.secret == null){
         return res.status(401).json({responsecode:401, responsemessage:'Unautorized'});
     }
